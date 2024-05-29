71c66feeb85fbfafd5f6211c715f2ffcc2670ba3
daniela0111 <95025799+daniela0111@users.noreply.github.com>
2024-05-29 10:14:23 +0200

    app

--- Wash_World/App.tsx
+++ Wash_World/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { store } from './store'; 
-import NavigationProvider from './Navigation'; 
+import { store } from './src/store'; 
+import NavigationProvider from './src/Navigation'; 
 
 const App: React.FC = () => {
   return (
