efae228a0713c56f71f912c7853dae6c44a969cc
daniela0111 <95025799+daniela0111@users.noreply.github.com>
2024-06-04 21:23:03 +0200

    Update LogInScreen.tsx

--- Wash_World/src/screens/LogInScreen.tsx
+++ Wash_World/src/screens/LogInScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
-import {}
+import { }
 //import { useDispatch } from 'react-redux';
 //import { setLogin } from '../reducers/authReducer'; 
 
